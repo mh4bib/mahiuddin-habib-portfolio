71c1e94703484c2a9e3740fcd3b80cc42344f3af
mahiuddinhabib <mh.habib137@gmail.com>
2022-09-06 01:24:46 +0600

    basic structure of contact section built

--- src/Components/Home/Contact.js
+++ src/Components/Home/Contact.js
@@ -15,15 +15,26 @@ const Contact = () => {
       });
   };
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input className='btn' type="submit" value="Send" />
-    </form>
+    <div className='bg-green-300'>
+      <div className='flex flex-col md:flex-row justify-center mx-4 py-10'>
+        <div className='w-[100%] md:w-[360px] bg-lime-200'>
+          <h2 className='text-2xl font-semibold pt-6'>Send Message</h2>
+          <form ref={form} onSubmit={sendEmail} className='text-left px-6 py-4'>
+            <label>Name</label><br />
+            <input type="text" name="user_name" className='w-full p-[5px] pl-2 mt-1 mb-3 focus:outline-none rounded' /><br />
+            <label>Email</label><br />
+            <input type="email" name="user_email" className='w-full p-[5px] pl-2 mt-1 mb-3 focus:outline-none rounded' /><br />
+            <label>Message</label><br />
+            <textarea name="message" rows={30} className='w-full p-[5px] pl-2 mt-1 mb-3 focus:outline-none rounded' /><br />
+            <input className='btn' type="submit" value="Send" />
+          </form>
+        </div>
+
+        <div className='w-[100%] md:w-[360px] bg-red-200'>
+          <h2>Contact Me</h2>
+        </div>
+      </div>
+    </div>
   );
 };
 
